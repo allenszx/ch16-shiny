6bc4652f50350b4c8ed9fac67a5f8da357a8c71f
applemac <applemac@macdeMacBook-Pro-5.local>
2018-05-10 21:59:32 -0700

    ch16 e

--- exercise-3/server.R
+++ exercise-3/server.R
@@ -3,10 +3,15 @@
 library(shiny)
 library(ggplot2)
 # Create a shiny server that creates a scatterplot. 
-
+server <- function(input, output) {
 # It should take as an input the x_var, y_var, color, and size
 # Save the result of `renderPlot` to output$scatter
-
+    output$scatter <- renderText({
+      x_data < mpg[[input$x_var]]
+      y_data < mpg[[input$y_var]]
+      p <- ggplot(mpg) +
+        geom_point(mapping = aes(x = x_data, y = y_data))
+    })
     # Store x and y values to plot
 
 
@@ -15,3 +20,5 @@ library(ggplot2)
     
     # Create ggplot scatter
 
+}
+shinyServer(server)
--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -5,21 +5,25 @@ library(shiny)
 select_values <- colnames(mpg)
 
 # Create a shinyUI with a `fluidPage` layout
-
+ui <- fluidPage(
   # A page header
-
-  
+  titlePanel("sdasdasdasd"),
   # Add a select input for the x variable
-
+  selectInput("x_var", "choose the x variable", choices = select_values),
+  
   
   # Add a select input for the y variable
-
-
+ 
+  selectInput("y_var", "choose the y variable", choices = select_values),
   # Add a sliderInput to set the size of each point
-
+  sliderInput("size", "choose the size", 1, 100, 10),
 
   # Add a selectInput that allows you to select a color from a list of choices
 
-
+  selectInput("color", "choose the color", choices = c("blue", "green")),
+  mainPanel(plotOutput("scatter"))
 
   # Plot the output with the name "scatter"
+)
+
+shinyUI(ui)
